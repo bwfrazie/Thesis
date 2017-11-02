142a40a20d2b7b8b9b916a55db826fda17c641ef
frazibw1 <frazibw1@frazibw1-ml2.jhuapl.edu>
2017-11-02 13:55:52 -0400

    updating test builder with JHU/APL title page

--- mfiles/generateSeaSurface.m
+++ mfiles/generateSeaSurface.m
@@ -15,21 +15,8 @@ N = 2*(length(k) - 1);
 S = Elfouhaily(k,U10,age);
 
 %create the random variables
-W = randn(1,N);
 
 V = [];
-% V(1) = sqrt(S(1)/2*dk)*W(1);
-% 
-% for (j = 2: N/2)
-%     V(j) = sqrt(1/2*S(j)*dk)*1/2*(W(2*j-1) + 1i*W(2*j));
-% end
-% 
-% V(N/2 + 1) = 0;
-% 
-% for (j = N/2+2:N)
-%     V(j) = conj(V(N-j + 2));
-% end
-
 w = randn(1,N/2);
 u = randn(1,N/2);
 
