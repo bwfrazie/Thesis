0e85d51f44e71982a22474149e78bbb38b92926e
shockwavefraz <bfrazier>
2017-11-05 20:59:09 -0500

    adding 2D slices to Spectral plots

--- mfiles/generate2DElfouhailySpectra.m
+++ mfiles/generate2DElfouhailySpectra.m
@@ -6,7 +6,9 @@ if nargin == 1
     saveFigs = varargin{1};
 end
 
-kx = linspace(-370,370,1000);
+dk = 0.0025;
+N = 4000;
+kx = (-N/2+1:N/2)*dk;
 ky = kx';
 
 [kxx,kyy] = meshgrid(kx,ky);
@@ -17,7 +19,8 @@ ky = kx';
 h(1) = figure('pos',[50 50 1000 400]);
 subplot(1,2,1)
 
-S = Elfouhaily2D(k,phi,20,0.84);
+S = Elfouhaily2D(k,phi,10,0.84);
+S1 = Elfouhaily(kx,10,0.84);
 imagesc(kx,ky,10*log10(S));%,[-150 30]);
 hold on
 
@@ -48,6 +51,18 @@ set(gca,'FontSize',12)
 set(gca,'FontWeight','bold')
 colormap(jet)
 colorbar
+
+h(2) = figure;%('pos',[50 50 1000 400]);
+Sx = S(N/2,:);
+Sy = S(:,N/2);
+loglog(kx(N/2:end),Sx(N/2:end));
+hold on
+loglog(ky(N/2:end),Sy(N/2:end));
+loglog(kx(N/2:end),S1(N/2:end));
+legend('\Phi_x','\Phi_y','S');
+xlim([0 370]);
+ylim([10^-15 10^3])
+grid on
 
 if(saveFigs)
  saveas(h(1),'elf_variance_curvature_spectrum.png','png')
