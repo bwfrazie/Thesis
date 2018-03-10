394b43bb50624194d4e86a6bfaebd4b5c3d05dc5
Ben Frazier <bwfrazier@gmail.com>
2018-03-10 09:37:40 -0500

    updating plots

--- mfiles/RunTEMPER/plotTdata.m
+++ mfiles/RunTEMPER/plotTdata.m
@@ -1,4 +1,4 @@
-% function plotTdata(tdata)
+function plotTdata(tdata)
  
 figure
 % subplot(1,2,1)
@@ -38,4 +38,23 @@ legend('10km','15km','20km','Location','Southeast')
 xlim([0 30])
 ylim([0 2])
 grid on
-% axis square
+
+figure
+aind5 = find(abs(tdata.h - 5) < 0.025);
+aind10 = find(abs(tdata.h - 10) < 0.025);
+aind20 = find(abs(tdata.h - 20) < 0.025);
+aind30 = find(abs(tdata.h - 30) < 0.025);
+plot(tdata.r,tdata.f(aind5,:),'LineWidth',2);
+hold on
+plot(tdata.r,tdata.f(aind10,:),'--','LineWidth',2);
+plot(tdata.r,tdata.f(aind20,:),'-.','LineWidth',2);
+plot(tdata.r,tdata.f(aind30,:),'-.','LineWidth',2);
+set(gca,'LineWidth',2)
+set(gca,'FontSize',12)
+set(gca,'FontWeight','bold')
+xlabel('Range (km)')
+ylabel('Propagation Factor')
+legend('5m','10m','20m','30m','Location','Southeast')
+xlim([0 30])
+ylim([0 2])
+grid on
