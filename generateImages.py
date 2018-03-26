35efd04adfcca279f85b0093543bb3d026c33e0b
Ben Frazier <bwfrazier@gmail.com>
2018-03-26 12:27:03 -0400

    adding path contour info

--- generateImages.py
+++ generateImages.py
@@ -12,3 +12,13 @@ while os.path.isfile('hankel_contours-figure0.pdf') == False:
 call(["cp", "hankel_contours-figure0.pdf", "../media/hankel_contours-figure0.pdf"])
 call(["cp", "hankel_contours-figure1.pdf", "../media/hankel_contours-figure1.pdf"])
 
+os.chdir('..')
+call(["cp", "src/path_contour.tex","build/path_contour.tex"])
+os.chdir('build')
+call(["pdflatex", "-shell-escape","path_contour.tex"])
+
+while os.path.isfile('path_contour-figure0.pdf') == False:
+	print('Waiting ...')
+	pass
+	
+call(["cp", "path_contour-figure0.pdf", "../media/path_contour-figure0.pdf"])
