201b986a8d8814b4360a91c9c9d2cebedfc69280
Ben Frazier <bwfrazier@gmail.com>
2018-05-16 10:21:32 -0400

    Adding wind direction angle to configuration and generation

--- python/generatePathStatistics.py
+++ python/generatePathStatistics.py
@@ -20,7 +20,7 @@ def main():
 	srfInputFilename = "surfaceinput.srf";
 	inputFilename = "base_input.in"	
 
-	L,N,U10,age,useFilter = getSeaSurfaceConfiguration(inputXMLFile)
+	L,N,U10,age,phi,useFilter = getSeaSurfaceConfiguration(inputXMLFile)
 	H,seed,nIter,temper,freq = getComputationConfiguration(inputXMLFile)
 	setupRunFolder(inputFilename,srfInputFilename,inputXMLFile,"data")
 	s = setupSeeds(nCores, seed)
@@ -29,11 +29,11 @@ def main():
 	filePrefix = str(int(U10)) + "mps_"
 	
 	print "Initializing Processes ..."
-	
+
 	proc = [ Process() for i in range(nCores)]
 	
 	for i in range (0,nCores):
-		proc[i] = Process(target = runTEMPERProcess, args = (L,N,U10,age,H,freq,useFilter,s[i], filePrefix, inputFilename,srfInputFilename,start[i],stop[i],temper,i+1))
+		proc[i] = Process(target = runTEMPERProcess, args = (L,N,U10,phi,age,H,freq,useFilter,s[i], filePrefix, inputFilename,srfInputFilename,start[i],stop[i],temper,i+1))
 		proc[i].start()
 	
 
