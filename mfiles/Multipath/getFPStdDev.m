5024a2a2cde25d058a3c11d21b3714b83380819d
Ben Frazier <bwfrazier@gmail.com>
2018-09-23 12:27:28 -0400

    Updating predicted results and comparisons

--- mfiles/Multipath/getFPStdDev.m
+++ mfiles/Multipath/getFPStdDev.m
@@ -1,14 +1,16 @@
-function sigma = getFPStdDev(L,h1,h2,sigmah,lambda)
+function [sigma,sigmad,sigmas] = getFPStdDev(L,h1,h2,sigmah,lambda,sf)
 
 k = 2*pi/lambda;
 f = 3.0e8/lambda;
 
+sf = 1/4;
 
 re = 6371000*4/3;
-graz = asin(h1./L*(1 + h1/(2*re)) - L/(2*re));
+
+graz = asin(h1./L.*(1 + h1/(2*re)) - L/(2*re));
 
 
-L0 = (h1+h2).^4./(h1*h2*L .^3);
+L0 = (h1+h2).^4./(h1*h2.*L .^3);
 
 gam = abs(getReflectionCoefficient( graz,sigmah,lambda));
 
@@ -17,6 +19,8 @@ xtwiddle = sqrt(2./(k*L0));
 sigmas = sin(k*2*h1*h2./L).^2;
 Q = 2*pi*f*(1-gam)./gam.^2;
 alpha = k*L./Q;
-sigmad = sqrt(1./(pi*alpha)).*(xtwiddle./L).^2;
+
+sigmad = sf*(1./(pi*alpha)).^(1/4).*(xtwiddle./L);
+% sigmad = sqrt(1./(pi*alpha)).*(xtwiddle./L).^2;
 
 sigma = sigmas.*sigmad;
--- mfiles/Multipath/plotMultipathVariance.m
+++ mfiles/Multipath/plotMultipathVariance.m
@@ -8,17 +8,19 @@ end
 
 f = 10e9;
 lambda = 3e8/10e9;
-sigmah = 0.01;
+sigmah = 0.65;
 
 
 h1 = 30;
 h2 = 20;
 L = linspace(1000,20000,10000);
 
-sigma_L = getFPStdDev(L,h1,h2,sigmah,lambda);
+[sigma_L,sigmad,sigmas]  = getFPStdDev(L,h1,h2,sigmah,lambda);
 
 hh1 = figure;
 plot(L/1000,sigma_L,'LineWidth',2)
+hold on
+plot(L/1000,sigmad,'LineWidth',2)
 grid on
 xlim([4 20])
 xlabel('Down Range Distance (km)')
@@ -32,10 +34,12 @@ set(gca,'FontWeight','bold')
 h2 = linspace(1,30,10000);
 L = 20000;
 
-sigma_a = getFPStdDev(L,h1,h2,sigmah,lambda);
+[sigma_a,sigmad,sigmas] = getFPStdDev(L,h1,h2,sigmah,lambda);
 
 hh2 = figure;
 plot(h2,sigma_a,'LineWidth',2)
+hold on
+plot(h2,sigmad,'LineWidth',2)
 grid on
 xlim([2 20])
 xlabel('Altitude (m)')
